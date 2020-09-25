01950d484b820e26869e033b7db915720f74ac1f
Rohan Pant <rohan.pant14@gmail.com>
2020-09-25 19:52:20 +0530

    Function as Operator

---
+++ Function as Operator.scala
@@ -0,0 +1,17 @@
+// Function as Operator
+
+def ^(x: Int, y: Int) = x ^ y;
+
+print(3^2)
+//--> 9
+
+
+def **(x: Int) = x * x;
+
+print(**3)
+//--> 9
+
+def ***(x: Int) = x * x * x;
+
+print(***(3))
+//--> 27
