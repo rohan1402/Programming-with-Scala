1b14b08c988472e72ec41ad96cd536348ce7062f
Rohan Pant <rohan.pant14@gmail.com>
2020-09-25 20:07:44 +0530

    Higher order functions

---
+++ Partially Applied Functions.scala
@@ -0,0 +1,25 @@
+// Partially Applied Functions
+
+val sum = (a: Int, b: Int, c: Int) = a + b + c
+
+val f1 = sum(10, _ : Int, _ : Int)
+println(f1(20, 30))
+// --> 60
+
+val f2 = sum(10, 20, _ : Int) 
+println(f2(30))
+//--> 60
+
+
+
+def log(date: Date, message: String) = {
+	println(date + " " + message)
+}
+
+val date = new Date;
+val newlog = log(date, _ : String)
+newlog("Message 1")
+// --> Mon Dec 04 22:44:15 "Message 1"
+
+newlog("Message 2")
+// --> Mon Dec 04 22:44:15 "Message 2"
